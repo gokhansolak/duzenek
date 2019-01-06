85075f3e73a1fa57722b4c4822a7c2d0399cd313
Gokhan Solak <gknsolak@gmail.com>
2019-01-06 15:52:21 +0000

    türkçe sort çalışmıyor

--- src/cekim_ayir.py
+++ src/cekim_ayir.py
@@ -1,7 +1,8 @@
-
+#!/usr/bin/python3
 import re
 import json
 
+ALFABE = ' \'-aâbcçdefgğhıîijklmnoöprsştuüvyz'
 YUMUSAT = {'ç': 'c', 't': 'd', 'p': 'b', 'k': 'ğ', 'g':'ğ'}
 SERTLES = {'c': 'ç', 'd': 't', 'b': 'p', 'g': 'k'}
 SESLILER = {'hepsi':'aeıioöuü',
@@ -49,9 +50,10 @@ if __name__ == "__main__":
         ptr = json.load(json_file)
     # kökler listesi
     with open('data/KOKLER.txt') as kok_file:
-        kokler = [line.split()[0] for line in kok_file]
+        kokler = [line.strip() for line in kok_file]
         # sort for binary search
-        kokler.sort()
+        sort_sira = {i:ALFABE.index(i) for i in ALFABE}
+        kokler.sort(key=sort_sira.get)
 
     print(kokler)
 
