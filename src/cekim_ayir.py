f38fa1b532eb08a154eed53396ec6ceb859f2368
Gokhan Solak <gknsolak@gmail.com>
2019-01-06 18:59:50 +0000

    Çekim eki grupları + fiil

--- src/cekim_ayir.py
+++ src/cekim_ayir.py
@@ -12,15 +12,10 @@ SESLILER = {'hepsi':'aeıioöuü',
     'dar':'ıiuü',
     'geniş': 'aeoö'}
 
-def cekim_ayir(kelime):
-    # type safety
-    if(type(kelime) is not str):
-        return
 
-    kelime_isle = arc.kucult_str(kelime)
-    # isim cekim
+def cekim_grup(kelime, grup):
     # reverse iterate: sondan başa
-    for ek in reversed(ptr["çekim"]["isim"]):
+    for ek in reversed(grup):
 
         ptr_ek = ek["örüntü"]
 
@@ -28,38 +23,71 @@ def cekim_ayir(kelime):
         ptr_ek += "$"
 
         # eşleştir
-        match = re.search(ptr_ek, kelime_isle)
+        match = re.search(ptr_ek, kelime)
         if match:
             # bilgilendir
             print(ek["tip"] + " eki bulundu: "+match.group(1))
             # ek temizle
-            kelime_isle = kelime_isle[:match.start()]
+            kelime = kelime[:match.start()]
             # eşleşen kök bak, varsa bitir
-            if arc.ikili_ara(kokler, kelime_isle) >= 0:
-                print("Kök bulundu: "+ kelime_isle)
-                return kelime_isle
+            if arc.ikili_ara(kokler_veri, kelime) >= 0:
+                print("Kök bulundu: "+ kelime)
+                return kelime, True
 
     # bulunamadıysa en küçük parçayı döndür
-    print("Sözlükte olmayan kök: " + kelime_isle)
-    return kelime_isle
+    print("Sözlükte olmayan kök: " + kelime)
+    return kelime, False
+
+def cekim_ayir(kelime):
+    # type safety
+    if(type(kelime) is not str):
+        return
+
+    kelime_isle = arc.kucult_str(kelime)
+    kokler_bulunan = []
+    # isim ve fiil cekim ekleri
+    for ek_tipi in ["isim", "fiil"]:
+        # ek tipi için kök ara
+        ek_grubu = oruntu_veri["çekim"][ek_tipi]
+        kok, mesru = cekim_grup(kelime_isle, ek_grubu)
+        # meşruysa (sözlükte bulunduysa) bitir
+        if mesru:
+            return kok
+        else:
+            kokler_bulunan += kok
+
+    # hiçbir ek tipiyle meşru kök bulunmadıysa en kısayı döndür
+    kok_kisa = None
+    for kok in kokler_bulunan:
+        if not kok_kisa:
+            kok_kisa = kok
+        elif len(kok) < len(kok_kisa):
+            kok_kisa = kok
+    return kok_kisa
 
 
 
 if __name__ == "__main__":
 
-    global ptr
-    global kokler
+    global oruntu_veri
+    global kokler_veri
 
     # ekler örüntü verisi
     with open('data/patterns.json') as json_file:
-        ptr = json.load(json_file)
+        oruntu_veri = json.load(json_file)
     # kökler listesi
     with open('data/KOKLER.txt') as kok_file:
-        kokler = [line.strip() for line in kok_file]
+        kokler_veri = [line.strip() for line in kok_file]
         # sort for binary search
-        kokler.sort()
+        kokler_veri.sort()
 
     cekim_ayir("Elmalarımızdakiyle")
     cekim_ayir("Evleriyle")
     cekim_ayir("Gözlerinin")
     cekim_ayir("İnsanlarca")
+    cekim_ayir("almıştık")
+    cekim_ayir("yapmayacaktınız")
+    cekim_ayir("gelmeliydiler")
+    cekim_ayir("sevmiyorsun")
+    cekim_ayir("koşmazsınız")
+    cekim_ayir("gezsinler")
